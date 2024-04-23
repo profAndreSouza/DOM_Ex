1610e02b11f9ff65a1263b8b41d7ed766bc5e129
Andre <andre.souza@sp.senai.br>
2024-04-23 20:33:30 -0300

    initial commit

---
+++ js/script.js
@@ -0,0 +1,44 @@
+const squadForm = document.getElementById("squadForm");
+
+squadForm.addEventListener("submit", function(event) {
+    event.preventDefault();
+
+    const squadName = document.getElementById("squadName").value;
+    const squadLeader = document.getElementById("squadLeader").value;
+    const squadMembers = document.getElementById("squadMembers").value;
+
+    const newRow = document.createElement("tr");
+
+    const tdName = document.createElement("td");
+    const tdLeader = document.createElement("td");
+    const tdMembers = document.createElement("td");
+
+    tdName.appendChild(document.createTextNode(squadName));
+    tdLeader.appendChild(document.createTextNode(squadLeader));
+    tdMembers.appendChild(document.createTextNode(squadMembers));
+
+    newRow.appendChild(tdName);
+    newRow.appendChild(tdLeader);
+    newRow.appendChild(tdMembers);
+
+    const tdAction = document.createElement("td");
+
+    const btEdit = document.createElement("button")
+    btEdit.textContent = "Editar";
+    btEdit.addEventListener("click", function () {
+        alert("Editar");
+    })
+    
+    const btDelete = document.createElement("button")
+    btDelete.textContent = "Remover";
+    btDelete.addEventListener("click", function () {
+        alert(this.rowIndex);
+    })
+
+    tdAction.appendChild(btEdit);
+    tdAction.appendChild(btDelete);
+
+    newRow.appendChild(tdAction);
+
+    document.getElementById("squadTable").appendChild(newRow);
+})
